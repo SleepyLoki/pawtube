8cb706fa96c11173d74d595d1e305c51c962a29f
Enrique Cerna <ecerna96@gmail.com>
2019-07-20 16:00:42 +0000

    set permissions to the lessons controller - lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+  
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  helper_method :current_course
+  def current_course
+      current_lesson.section.course
+  end
+
+  def require_authorized_for_current_course
+    if ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Must Be Enrolled in Course to Access this Lesson'
+    end
   end
 end
